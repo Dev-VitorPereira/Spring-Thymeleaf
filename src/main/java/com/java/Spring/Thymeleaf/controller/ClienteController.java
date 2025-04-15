6417b5b277ca4f0f2b863cf3ada01fb0d02da160
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-15 11:02:15 -0300

    feat: Finalizado Controllers e Serivces e configurado BD

--- src/main/java/com/java/Spring/Thymeleaf/controller/ClienteController.java
+++ src/main/java/com/java/Spring/Thymeleaf/controller/ClienteController.java
@@ -1,11 +1,52 @@
 package com.java.Spring.Thymeleaf.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.java.Spring.Thymeleaf.model.Cliente;
+import com.java.Spring.Thymeleaf.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
 
 public class ClienteController {
 
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public String listarCliente(Model model){
+        List<Cliente> clientes = clienteService.listarClientes();
+        model.addAttribute("cliente",clientes);
+        return "clientes/listar";
+    }
+
+    @GetMapping("/salvar")
+    public String salvarCliente(@ModelAttribute Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:clientes";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarAlunoForm(@PathVariable Long id, Model model) {
+        Cliente cliente = clienteService.buscarId(id)
+                .orElseThrow(() -> new RuntimeException("Aluno não encontrado com o ID: " + id));
+        model.addAttribute("aluno", cliente);
+        return "clientes/form";
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public String atualizarCliente(@PathVariable Long id, @ModelAttribute Cliente cliente) {
+        clienteService.atualizarCliente(id, cliente);
+        return "redirect:/clientes";
+    }
+
+
+    @GetMapping("/deletar/{id}")
+    public String deletarCliente(@PathVariable Long id) {
+        clienteService.deletar(id);
+        return "redirect:/clientes";
+    }
 }

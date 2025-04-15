8f41ce2c0a14e3d91a756f4838d5474d554ffb1b
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-15 10:37:52 -0300

    feat: Criando controller cliente

---
+++ src/main/java/com/java/Spring/Thymeleaf/controller/ClienteController.java
@@ -0,0 +1,13 @@
+package com.java.Spring.Thymeleaf.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+
+public class ClientController {
+
+}
---
+++ src/main/java/com/java/Spring/Thymeleaf/service/ClienteService.java
@@ -0,0 +1,15 @@
+package com.java.Spring.Thymeleaf.service;
+
+import com.java.Spring.Thymeleaf.model.Client;
+import com.java.Spring.Thymeleaf.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client salve
+}

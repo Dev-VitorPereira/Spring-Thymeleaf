b2cd88997855e298479ec57e3ee2cd67be972392
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-15 09:49:24 -0300

    feat: criado entidade "cliente"

--- src/main/java/com/java/Spring/Thymeleaf/model/Client.java
+++ src/main/java/com/java/Spring/Thymeleaf/model/Client.java
@@ -1,4 +1,26 @@
 package com.java.Spring.Thymeleaf.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table (name = "cliente")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Client {
+    @Id
+    @GeneratedValue (strategy = GenerationType.UUID)
+    Long id;
+
+    private String nome;
+    private int idade;
+    private String email;
+
 }
